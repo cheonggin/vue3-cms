ed62772bb8f81432f4406abd1d1c97bf582d29a2
gin <215129113@qq.com>
2021-11-26 22:45:40 +0800

    feat(breadcrumb): breadcrumb组件的二次封装

---
+++ src/base-ui/breadcrumb/breadcrumb.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-breadcrumb :separator-icon="ArrowRight">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in $route.meta" :key="index">
+      {{ item }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script setup>
+import { ArrowRight } from '@element-plus/icons'
+</script>
+
+<style lang="scss" scoped></style>
