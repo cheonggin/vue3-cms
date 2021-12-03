10f4180dffe949a43756f1df9ae653dc08bbd9c0
gin <215129113@qq.com>
2021-12-03 12:25:30 +0800

    style(user): 修改user中表结构的prop

--- src/views/main/user/config/table.config.js
+++ src/views/main/user/config/table.config.js
@@ -1,6 +1,6 @@
 export const contentTableConfig = {
   tableColumn: [
-    { prop: 'username', label: '用户名', minwidth: 100 },
+    { prop: 'name', label: '用户名', minwidth: 100 },
     { prop: 'status', label: '状态', minwidth: 100 },
     { prop: 'created', label: '创建时间', minwidth: 100 },
     { prop: 'updated', label: '更新时间', minwidth: 100 }
