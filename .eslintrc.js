db8075bbd9e31a9d554f337f57a5554b5b14c08e
gin <215129113@qq.com>
2021-12-10 11:02:03 +0800

    feat(eslint): 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     'vue/multi-word-component-names': 'off',
     'no-undef': 'off',
-    'vue/v-on-event-hyphenation': 'off'
+    'vue/v-on-event-hyphenation': 'off',
+    'array-callback-return': 'off'
   }
 }
