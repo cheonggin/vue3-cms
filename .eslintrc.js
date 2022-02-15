1b74f4a0d02d84b05b5c75469583f6204a3adab6
gin <215129113@qq.com>
2022-02-15 23:55:20 +0800

    refactor(refactor): init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:vue/essential', 'standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-multiple-template-root': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>App</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
