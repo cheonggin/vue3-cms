b9207910ac4e7078c054758a17f94fa5b07dda0c
gin <215129113@qq.com>
2022-02-16 09:56:03 +0800

    feat(element-plus): element-plus按需自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<template>App</template>
+<template>
+  <ElConfigProvider :locale="zhCn">App</ElConfigProvider>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+</script>
 
 <style scoped></style>
