79c5f2855bd99eb29fa8d17fb81784c77b4455c9
gin <215129113@qq.co>
2022-03-11 11:29:22 +0800

    refactor(admin): 修改admin配置

--- src/views/main/system/admin/config/form.config.ts
+++ src/views/main/system/admin/config/form.config.ts
@@ -2,9 +2,26 @@ import { IFormConfig } from '@/base-ui/form'
 
 export const formConfig: IFormConfig = {
   formData: [
-    { field: 'name', label: '管理员名称', type: 'input' },
-    { field: 'role_id', label: '担任角色', type: 'select', options: [] },
-    { field: 'password', label: '密码', type: 'password', isHidden: false }
+    {
+      field: 'name',
+      label: '管理员名称',
+      type: 'input',
+      placeholder: '请输入管理员名称'
+    },
+    {
+      field: 'role_id',
+      label: '担任角色',
+      type: 'select',
+      options: [],
+      placeholder: '请选择角色'
+    },
+    {
+      field: 'password',
+      label: '密码',
+      type: 'password',
+      isHidden: false,
+      placeholder: '请输入管理员密码'
+    }
   ],
   span: 24,
   labelPosition: 'left'
